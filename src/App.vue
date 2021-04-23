8bae1576088f42f789cb1a27329b8eb6dd55c24b
oliverobrien780 <83093430+oliverobrien780@users.noreply.github.com>
2021-04-24 03:21:59 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar />
+  <Token />
+  <Tvl />
+  <Footer />
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Token from "./components/Token";
+import Tvl from "./components/Tvl";
+
+
+export default {
+  name: "App",
+  components: {
+    Navbar: Navbar,
+    Footer: Footer,
+    Token: Token,
+    Tvl: Tvl,
+  },
+};
+</script>
